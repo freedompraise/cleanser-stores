29c647bcb6ea11ac995bca2e4f10be008961bc64
Praise Freedom Dike <dikepraise119@gmail.com>
2023-02-11 11:34:01 +0100

    icon.png

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -22,6 +22,5 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('store.urls')),
-    path('apis/', include('store.apis.urls')),
     path('paypal/', include('paypal.standard.ipn.urls')),
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
